4d7b76fff6c8b7404e74e54781964bf4aeb969f6
Beto-sa <adalbertosilvaalmeida@gmail.com>
2026-05-02 22:57:27 -0300

    verificador de aprovação

--- MediaAcademica.py
+++ MediaAcademica.py
@@ -9,4 +9,9 @@ nota2 = float(input("Informe a segunda nota do aluno:")) # Recebe a segunda nota
 media = (nota1 + nota2) / 2 # Faz o calculo da média
 print("A média final de %s é %f." % (aluno,media)) # Imprime a média do aluno.
 
-#test github
+if media < 4.0:
+    print("%s foi REPROVADO." %aluno)
+elif media >=4.0 and media < 7.0:
+    print("%s deverá fazer a prova final." %aluno)
+else:
+    print("%s foi APROVADO." %aluno)
