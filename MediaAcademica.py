28f1817faf96e410a7762870a58dbcf86e230966
Beto-sa <adalbertosilvaalmeida@gmail.com>
2026-05-03 09:25:46 -0300

    Finaloização do código.

--- MediaAcademica.py
+++ MediaAcademica.py
@@ -1,4 +1,4 @@
-#ALUNO: ADALBERTO DA SILVA ALMEIDA
+#DESENVOLVEDOR: ADALBERTO DA SILVA ALMEIDA :)
 
 # CALCULADORA DE MÉDIA ACADÊMICA #
 
@@ -11,7 +11,7 @@ nota2 = float(input("Informe a segunda nota do aluno:")) # Recebe a segunda nota
 '''Mostrar a média do aluno'''
 
 media = (nota1 + nota2) / 2 # Faz o calculo da média
-print("A média de %s é %f." % (aluno,media)) # Imprime a média do aluno.
+print("A média de %s é %.2f" % (aluno,media)) # Imprime a média do aluno.
 
 
 '''Verificar o estatus dos aluno e a média final'''
@@ -22,19 +22,21 @@ elif media >=4.0 and media < 7.0: # Verifica se a média do aluno é iguau ou ma
     minMedia = 5 - media # Calcula a média minima para a final.
     if minMedia < 4.0: # Ajusta a média minima para a final, que n pode ser menor que 4.
         minMedia = 4.0
-    print("%s deverá fazer a prova final e a média minima é %f." % (aluno, minMedia))
+    print("%s deverá fazer a prova final e a média minima é %.2f." % (aluno, minMedia))
 else: # Caso a media não atenda aos requisitos anteriores ela será maior que 7 logo o else será executado.
     print("%s foi APROVADO." %aluno)
 
 '''Receber nota final e calcular média final'''
 
-notaFinal = float(input("Digite a nota obtifa da final:"))
-mediaFinal = (notaFinal + media) / 2
-print("A media final foi : %f." %media)
+notaFinal = float(input("Digite a nota obtida na final:")) # Recebe a nota final.
+mediaFinal = float((notaFinal + media) / 2) # Calcula a media final.
+print("A media final foi : %.2f" %mediaFinal) # Imprime a média final.
 
 '''Avaliação após a prova final'''
 
-if mediaFinal >= 5.0:
+if mediaFinal >= 5.0: # Verifica se após a media final o aluno fou reprovado ou aprovado.
     print("%s foi APROVADO!" %aluno)
 else:
     print("%s foi REPROVADO!" %aluno)
+
+    '''     FIM :)     '''
