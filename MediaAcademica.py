914b35cf32a9707fe8af476ecb9c269b8be33906
Beto-sa <adalbertosilvaalmeida@gmail.com>
2026-05-02 21:06:32 -0300

    teste

--- MediaAcademica.py
+++ MediaAcademica.py
@@ -8,3 +8,5 @@ nota2 = float(input("Informe a segunda nota do aluno:")) # Recebe a segunda nota
 
 media = (nota1 + nota2) / 2 # Faz o calculo da média
 print("A média final de %s é %f." % (aluno,media)) # Imprime a média do aluno.
+
+#test github
